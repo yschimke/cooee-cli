f40fa959f369a2739bdddfb970c18691fd3909c0
Yuri Schimke <yuri@schimke.ee>
2018-12-15 15:16:11 +0000

    Build from github

--- cooee-cli.rb
+++ cooee-cli.rb
@@ -2,13 +2,18 @@ class CooeeCli < Formula
   desc "Coo.ee CLI"
   homepage "https://github.com/yschimke/cooee-cli"
   version "0.1"
-  url "file:///Users/yuri/workspace/cooee-cli/build/distributions/cooee-cli-master-ed2a449.tar"
+  # url "file:///Users/yuri/workspace/cooee-cli/build/distributions/cooee-cli-master-ed2a449.tar"
 #   sha256 "97c304c18a89fcbe59e2d0f8d6767f777ec811529713ca8796c93c41b6cb566d"
+#   head "https://github.com/yschimke/cooee-cli.git"
+
+  # url "https://github.com/yschimke/cooee-cli.git"
 
   depends_on :java
 
   def install
-    libexec.install Dir["*"]
+    system "./gradlew", "installDist"
+
+    libexec.install Dir["build/install/cooee-cli*/*"]
     bin.install_symlink "#{libexec}/bin/cooee-cli" => "cooee"
     bash_completion.install "#{libexec}/bash/completion.bash" => "cooee"
   end
