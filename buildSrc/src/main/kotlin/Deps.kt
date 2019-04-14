b97d3be67743814455898882c42019ce3ca3bed1
Yuri Schimke <yuri@schimke.ee>
2019-04-14 14:31:45 +0100

    Updates

--- buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -1,28 +1,3 @@
 object Versions {
-  val junit = "5.3.2"
-  val kotlin = "1.3.21"
-  val kotlinCoroutines = "1.1.0"
-  val moshiVersion = "1.8.0"
-  val okhttp = "3.13.1"
-  val oksocialOutput = "4.26.0"
-  val okurl = "1.80.0"
-  val okio = "2.2.2"
-}
-
-object Deps {
-  val airline2 = "com.github.rvesse:airline:2.6.0"
-  val coroutinesCore = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.kotlinCoroutines}"
-  val junitJupiterEngine = "org.junit.jupiter:junit-jupiter-engine:${Versions.junit}"
-  val kotlinReflect = "org.jetbrains.kotlin:kotlin-reflect:${Versions.kotlin}"
-  val kotlinStandardLibrary = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:${Versions.kotlin}"
-  val kotlinTest = "org.jetbrains.kotlin:kotlin-test:${Versions.kotlin}"
-  val kotlinTestJunit = "org.jetbrains.kotlin:kotlin-test-junit:${Versions.kotlin}"
-  val moshi = "com.squareup.moshi:moshi:${Versions.moshiVersion}"
-  val moshiAdapters = "com.squareup.moshi:moshi-adapters:${Versions.moshiVersion}"
-  val moshiKotlin = "com.squareup.moshi:moshi-kotlin:${Versions.moshiVersion}"
-  val okhttp = "com.squareup.okhttp3:okhttp:${Versions.okhttp}"
-  val okio = "com.squareup.okio:okio:${Versions.okio}"
-  val oksocialOutput = "com.baulsupp:oksocial-output:${Versions.oksocialOutput}"
-  val okurl = "com.baulsupp:okurl:${Versions.okurl}"
-  val slf4jJdk14 = "org.slf4j:slf4j-jdk14:1.8.0-beta2"
+  val kotlin = "1.3.30"
 }
