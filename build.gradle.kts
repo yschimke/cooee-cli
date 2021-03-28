8e0151f94ea7e0d424a06bd5144e0745bbda07c4
Yuri Schimke <yuri@schimke.ee>
2021-03-28 20:13:36 +0100

    Fix publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -130,6 +130,20 @@ dependencies {
   testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.5.2")
 }
 
+val sourcesJar by tasks.registering(Jar::class) {
+  classifier = "sources"
+  from(sourceSets.main.get().allSource)
+}
+
+publishing {
+  publications {
+    register("mavenJava", MavenPublication::class) {
+      from(components["java"])
+      artifact(sourcesJar.get())
+    }
+  }
+}
+
 if (properties.containsKey("graalbuild")) {
   val nativeImage = tasks["nativeImage"]
 
